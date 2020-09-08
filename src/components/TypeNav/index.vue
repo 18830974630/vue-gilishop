88b246a5d770a01af31acf1d6d137aa8fea3477d
18830974630 <691577405@qq.com>
2020-09-08 16:49:53 +0800

    l

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -4,7 +4,7 @@
       <div>
         <h2 class="all">全部商品分类</h2>
         <div class="sort">
-          <div class="all-sort-list2">
+          <div class="all-sort-list2" @click="toSearch">
             <div
               class="item"
               v-for="(c1, index) in categoryList"
@@ -13,26 +13,41 @@
               @mouseenter="moveIn(index)"
             >
               <h3>
-                <router-link
+                <!-- <router-link
                   :to="{name:'search',query:{categoryName:c1.categoryName,category1id:c1.category1id }}"
-                >{{c1.categoryName}}</router-link>
+                >{{c1.categoryName}}</router-link>-->
                 <!-- <a href>{{c1.categoryName}}</a> -->
+                <a
+                  href="javascript:;"
+                  :data-categoryName="c1.categoryName"
+                  :data-category1Id="c1.categoryId"
+                >{{c1.categoryName}}</a>
               </h3>
               <div class="item-list clearfix">
                 <div class="subitem">
                   <dl class="fore" v-for="(c2) in c1.categoryChild" :key="c2.categoryId">
                     <dt>
-                      <router-link
+                      <!-- <router-link
                         :to="{name:'search',query:{categoryName:c2.categoryName,category1id:c2.category1id}}"
-                      >{{c2.categoryName}}</router-link>
+                      >{{c2.categoryName}}</router-link>-->
                       <!-- <a href>{{c2.categoryName}}</a> -->
+                      <a
+                        href="javascript:;"
+                        :data-categoryName="c2.categoryName"
+                        :data-category2Id="c2.categoryId"
+                      >{{c2.categoryName}}</a>
                     </dt>
                     <dd>
                       <em v-for="(c3) in c2.categoryChild" :key="c3.categoryId">
-                        <router-link
+                        <!-- <router-link
                           :to="{name:'search',query:{categoryName:c3.categoryName,}}"
-                        >{{c3.categoryName}}</router-link>
+                        >{{c3.categoryName}}</router-link>-->
                         <!-- <a href>{{c3.categoryName}}</a> -->
+                        <a
+                          href="javascript:;"
+                          :data-categoryName="c3.categoryName"
+                          :data-category3Id="c3.categoryId"
+                        >{{c3.categoryName}}</a>
                       </em>
                     </dd>
                   </dl>
@@ -58,6 +73,7 @@
 
 <script>
 import { mapState, mapActions, mapGetters, mapMutations } from "vuex";
+import throttle from "lodash/throttle";
 export default {
   name: "TypeNav",
   data() {
@@ -72,9 +88,49 @@ export default {
     getCategoryList() {
       this.$store.dispatch("getCategoryList");
     },
-    moveIn(index) {
-      console.log(index);
-      this.currentIndex = index;
+    // moveIn(index) {
+    //   console.log(index);
+    //   this.currentIndex = index;
+    // },
+    moveIn: throttle(
+      function (index) {
+        // console.log(index);
+        this.currentIndex = index;
+      },
+      50,
+      { trailing: false }
+    ),
+    toSearch(event) {
+      let target = event.target; //代表目标元素  目标元素有可能是a 也有可能不是a
+      let data = target.dataset; //dataset 拿的就是元素身上以data-开头的所有的属性和值组成的一个对象 里面的属性都改为了小写
+      // console.log(data)
+
+      let { categoryname, category1id, category2id, category3id } = data;
+
+      if (categoryname) {
+        //如果categoryname是存在的代表点的一定是a
+
+        //既然点的是a 那么一定会跳转，所以我们创建跳转的对象
+        let location = {
+          name: "search",
+        };
+
+        //创建query参数的对象，来收集整理query参数
+        let query = {
+          categoryName: categoryname,
+        };
+        if (category1id) {
+          query.category1Id = category1id;
+        } else if (category2id) {
+          query.category2Id = category2id;
+        } else {
+          query.category3Id = category3id;
+        }
+        //把query参数放到location当中
+        location.query = query;
+
+        this.$router.push(location);
+      }
     },
   },
   computed: {
